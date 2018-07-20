b653f77439714108194ceb549d814a78c77baf21
Philipp Paulweber <p.paulweber@gmail.com>
2018-07-20 12:50:31 +0200

    Happy New Year :tada:

--- ci.sh
+++ ci.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-#   
-#   Copyright (c) 2016 Philipp Paulweber
+#
+#   Copyright (C) 2016-2018 Philipp Paulweber
 #   All rights reserved.
-#   
+#
 #   Developed by: Philipp Paulweber
-#                 https://github.com/ppaulweber/ci
-#   
+#                 <https://github.com/ppaulweber/ci>
+#
 #   This file is part of ci.
-#   
+#
 #   ci is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
-#   
+#
 #   ci is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #   GNU General Public License for more details.
-#   
+#
 #   You should have received a copy of the GNU General Public License
 #   along with ci. If not, see <http://www.gnu.org/licenses/>.
-#   
+#
 
 app=`basename $0`
 cwd=`pwd -P`
