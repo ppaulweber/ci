84c2008fb5cb9c853b4fe470a2df3b7598509282
Philipp Paulweber <p.paulweber@gmail.com>
2018-07-20 14:40:07 +0200

    Script: updated 'main' procedure and 'update' handling

--- ci.sh
+++ ci.sh
@@ -84,13 +84,65 @@ app=`basename $0`
 cwd=`pwd -P`
 dir=`dirname $(realpath $0)`
 
+function main
+{
+    if [ "$dir" != "$cwd" ]; then
+	error "$app has to be executed inside its root directory!"
+    fi
+
+    if [ ! -f $bin ]; then
+	warning "'$bin' not found, trying to fetch latest version"
+	update
+    fi
+
+    local cmd=$1
+    if [ -z "$cmd" ]; then
+	error "no 'command' argument provided"
+    fi
+
+    local cfg=$2
+    if [ -z "$cfg" ]; then
+	cfg=$cfg_default
+	info "no 'config' argument provided, using default config '$cfg'"
+
+	if [ ! -f $cfg ]; then
+            info "no default config found, creating a skeleton"
+            cp $cfg_example $cfg_default
+	fi
+    fi
+
+    if [ ! -f $cfg ]; then
+	error "unable to load config file '$cfg'"
+    fi
+
+    source $cfg
+
+
+    ## TODO: FIXME: @ppaulweber: add config file checks here!!!
+
+
+    if [ "$cmd" = "remove" ]; then
+	$cmd
+    elif [ "$cmd" = "update" ]; then
+	$cmd
+    elif [ "$cmd" = "server" ]; then
+	$cmd
+    elif [ "$cmd" = "worker" ]; then
+	$cmd
+    elif [ "$cmd" = "retire" ]; then
+	$cmd
+    else
+	error "invalid command '$cmd' provided"
+    fi
+
+}
 
 function remove
 {
     if [ -d $bin_path ]; then
-        rm -f $bin_path/*
-        rmdir $bin_path
-        message "removed all concourse binaries from path '$bin_path'"
+	rm -f $bin_path/*
+	rmdir $bin_path
+	message "removed all concourse binaries from path '$bin_path'"
     fi
 }
 
@@ -98,24 +150,31 @@ function remove
 function update
 {
     mkdir -p $bin_path
-    
-    tag=`curl -s -# ${url}/latest | egrep -o 'tag/(.*)\">' | sed 's/tag\///' | sed 's/\">//'`
-    info "latest version ${tag}"
+
+    local tag=`curl -s -# ${url}/latest | egrep -o 'tag/(.*)\">' | sed 's/tag\///' | sed 's/\">//'`
+    local bin_tag=${bin_path}/`echo ${tag} | sed "s/v//g"`
+
+    if [ -e ${bin_tag} ]; then
+	info "latest version ${tag} already installed"
+	return
+    fi
+
+
     wget -q --show-progress ${url}/download/${tag}/${file}
 
     chmod 755 $file
-    version=`./$file -v`
+    local version=`./$file -v`
     mv $file $version
-    
+
     rm -f $bin
     ln -s $version $bin_name
     mv $version  $bin_path
     mv $bin_name $bin_path
-    
+
     if [ ! -f $bin ]; then
-        error "'$bin' could not be installed"
+	error "'$bin' could not be installed"
     fi
-    
+
     message "updated to concourse version '$version' for '$plat' and '$arch'"
 }
 
@@ -123,8 +182,10 @@ function update
 function check_if_bin_exists_else_fetch_it
 {
     if [ ! -f $bin ]; then
-        warning "'$bin' not found, trying to fetch latest version"
-        update
+	warning "'$bin' not found, trying to fetch latest version"
+	update
+    else
+	message "concourse version ${tag}"
     fi
 }
 
@@ -134,7 +195,7 @@ function server
     check_if_bin_exists_else_fetch_it
 
     message "starting concourse server"
-    
+
     $bin web \
          --github-auth-client-id=$github_auth_client_id \
          --github-auth-client-secret=$github_auth_client_secret \
@@ -167,6 +228,8 @@ function retire
 {
     check_if_bin_exists_else_fetch_it
 
+    source $cfg
+
     message "retire concourse worker"
 
     $bin retire-worker \
@@ -175,56 +238,7 @@ function retire
          --tsa-worker-private-key $worker_key_private
 }
 
-
-
-if [ "$dir" != "$cwd" ]; then
-    error "$app has to be executed inside its root directory!"
-fi
-
-if [ ! -f $bin ]; then
-    warning "'$bin' not found, trying to fetch latest version"
-    update
-fi
-
-cmd=$1
-if [ -z "$cmd" ]; then
-    error "no 'command' argument provided"
-fi
-
-cfg=$2
-if [ -z "$cfg" ]; then
-    cfg=$cfg_default
-    info "no 'config' argument provided, using default config '$cfg'"
-
-    if [ ! -f $cfg ]; then
-        info "no default config found, creating a skeleton"
-        cp $cfg_example $cfg_default
-    fi
-fi
-
-if [ ! -f $cfg ]; then
-    error "unable to load config file '$cfg'"
-fi
-
-source $cfg
-
-
-## TODO: FIXME: @ppaulweber: add config file checks here!!!
-
-
-if [ "$cmd" = "remove" ]; then
-    $cmd
-elif [ "$cmd" = "update" ]; then
-    $cmd
-elif [ "$cmd" = "server" ]; then
-    $cmd
-elif [ "$cmd" = "worker" ]; then
-    $cmd
-elif [ "$cmd" = "retire" ]; then
-    $cmd
-else
-    error "invalid command '$cmd' provided"
-fi
+main $*
 
 
 # cd jq
